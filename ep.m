350163855075dd26d41efce862dccc49321eda18
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-25 11:02:50 -0200

    Blur\!

--- ep.m
+++ ep.m
@@ -33,3 +33,10 @@ function x = convolution(f,g)
     endfor
   endfor
 endfunction
+
+function x = blur(img)
+  w = [1/16 2/16 1/16;
+       2/16 4/16 2/16;
+       1/16 2/16 1/16];
+  x = convolution(img, w);
+endfunction
