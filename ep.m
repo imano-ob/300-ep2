925ad0031cc28460f20feebdc70339f5abd47f47
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-26 10:58:21 -0200

    Bordas arrumadas porcamente na convolução

--- ep.m
+++ ep.m
@@ -28,8 +28,29 @@ function x = convolution(f,g)
   x = f;
   for i = 1:size(f)(1)
     for j = 1:size(f)(2)
-      #TODO: Fix para casos nas fronteiras
-      x(i, j) = sum(sum( f(i-1:i+1, j-1:j+1) .* g));
+      #Eu odeio esse teco. Pensar em um jeito menos feio de fazer.
+      if i == 1
+        beginl = 0
+      else
+        beginl = -1
+      endif
+      if i == size(f)(1)
+        endl = 0
+      else
+        endl = 1
+      endif
+      if j == 1
+        beginc = 0
+      else
+        beginc = -1
+      endif
+      if j == size(f)(2)
+        endc = 0
+      else
+        endc = 1
+      endif
+      #Eu não me orgulho dessa linha...
+      x(i, j) = sum(sum( f(i+beginl:i+endl, j+beginc:j+endc) .* g(beginl+1:endl+1, beginc+1:endc+1));
     endfor
   endfor
 endfunction
