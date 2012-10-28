8842ade2bc674f3f9f191937e60b38576f131bbd
Renan T. Carneiro <imano_ob@uspgamedev.org>
2012-10-28 14:51:09 -0200

    Fix em falta de parenteses a erro de indexacao. WHY IS OCTAVE 1 INDEXED? AAAAAAAAAAAAAAAAaa

--- convolution.m
+++ convolution.m
@@ -4,27 +4,27 @@ function x = convolution(f,g)
     for j = 1:size(f)(2)
       #Eu odeio esse teco. Pensar em um jeito menos feio de fazer.
       if i == 1
-        beginl = 0
+        beginl = 0;
       else
-        beginl = -1
+        beginl = -1;
       endif
       if i == size(f)(1)
-        endl = 0
+        endl = 0;
       else
-        endl = 1
+        endl = 1;
       endif
       if j == 1
-        beginc = 0
+        beginc = 0;
       else
-        beginc = -1
+        beginc = -1;
       endif
       if j == size(f)(2)
-        endc = 0
+        endc = 0;
       else
-        endc = 1
+        endc = 1;
       endif
       #Eu não me orgulho dessa linha...
-      x(i, j) = sum(sum( f(i+beginl:i+endl, j+beginc:j+endc) .* g(beginl+1:endl+1, beginc+1:endc+1));
+      x(i, j) = sum(sum( f(i+beginl:i+endl, j+beginc:j+endc) .* g(beginl+2:endl+2, beginc+2:endc+2)));
     endfor
   endfor
 endfunction
