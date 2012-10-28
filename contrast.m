f29e3839e963341f286a24953907c4d09dbd96b8
Renan T. Carneiro <imano_ob@uspgamedev.org>
2012-10-28 14:15:07 -0200

    Por um lado, contrast devolve algo. Por outro lado, ta muito errado.

--- contrast.m
+++ contrast.m
@@ -1,24 +1,25 @@
 function x = contrast(img)
-  intensityProb(255) = 0;
+  intensityProb(256) = 0;
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
-      ++intensityProb(img(i, j));
+      #WHY IS OCTAVE 1 INDEXED INSTEAD OF 0 INDEXED?
+      ++intensityProb(img(i, j)+1);
     endfor
   endfor
-  intensityProb /= numel(img);
-  fda(1) = intensityProb(1);
-  for i = 2:255
+  intensityProb ./= numel(img);
+    fda(1) = intensityProb(1);
+  for i = 2:256
     fda(i) = fda(i-1) + intensityProb(i);
   endfor
   fdamin = min(fda);
   # fdamax = max(fda);
-  for i = 1:255
-    h(i) = round ((fda - fdamin) / (numel(img) - fdamin) * 255);
+  for i = 1:256
+    h(i) = round ((fda(i) - fdamin) / (numel(img) - fdamin) * 255);
   endfor
   x = img;
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
-      x(i, j) = h(x(i,j));
+      x(i, j) = h(x(i,j)+1)-1;
     endfor
   endfor
 endfunction
