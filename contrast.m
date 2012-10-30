ef69245efded4726dcf72c7f0d4ef9e311bbf292
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-30 15:14:39 -0200

    Contrast mais quieto agora.

--- contrast.m
+++ contrast.m
@@ -2,29 +2,21 @@ function x = contrast(img)
   intensityProb(256) = 0;
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
-      #WHY IS OCTAVE 1 INDEXED INSTEAD OF 0 INDEXED?
       ++intensityProb(img(i, j)+1);
     endfor
   endfor
-  intensityProb .*= 1.0
-  intensityProb ./= numel(img)
     fda(1) = intensityProb(1);
   for i = 2:256
     fda(i) = fda(i-1) + intensityProb(i);
   endfor
-  fdamin = min(fda)
-  fdamax = max(fda);
+  fdamin = min(fda);
   for i = 1:256
-    l = (fda(i) - fdamin) * 255
-    k = fdamax - fdamin
-    #k = (numel(img) - fdamin)
-    m = l/k
-    h(i) = round ( m)
+    h(i) = round ( (fda(i) - fdamin) / (numel(img) - fdamin) * 255);
   endfor
   x = img;
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
-      x(i, j) = h(x(i,j)+1)-1;
+      x(i, j) = h (x (i,j) + 1);
     endfor
   endfor
 endfunction
