7bd5445461431a3da3291fd151853abe5f971d36
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-30 15:15:10 -0200

    Convolução ignoa bordas agora

--- convolution.m
+++ convolution.m
@@ -1,30 +1,8 @@
 function x = convolution(f,g)
   x = f;
-  for i = 1:size(f)(1)
-    for j = 1:size(f)(2)
-      #Eu odeio esse teco. Pensar em um jeito menos feio de fazer.
-      if i == 1
-        beginl = 0;
-      else
-        beginl = -1;
-      endif
-      if i == size(f)(1)
-        endl = 0;
-      else
-        endl = 1;
-      endif
-      if j == 1
-        beginc = 0;
-      else
-        beginc = -1;
-      endif
-      if j == size(f)(2)
-        endc = 0;
-      else
-        endc = 1;
-      endif
-      #Eu não me orgulho dessa linha...
-      x(i, j) = sum(sum( f(i+beginl:i+endl, j+beginc:j+endc) .* g(beginl+2:endl+2, beginc+2:endc+2)));
+  for i = 2:size(f)(1)-1
+    for j = 2:size(f)(2)-1
+    x(i, j) = sum(sum( f(i-1:i+1, j-1:j+1) .* g));
     endfor
   endfor
 endfunction
