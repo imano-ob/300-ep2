880de8631634f6419c9b84ac990ce549cef2f4e0
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-30 16:10:27 -0200

    Main existe e funciona

--- ep.m
+++ ep.m
@@ -1,17 +1,25 @@
+#!/usr/bin/octave
 
-#imgname = arg
-#img = imread(imgname)
-#switch
-#case contrast
-#finalimg = contrast(img)
-#case blur
-#finalimg = blur(img)
-#case sharpen
-#finalimg = sharpen(img)
-#default
-#xinga user
-#vaza
-#endcase
-#finalimgname = fixname
-#write
-#huzzah!  
+args = argv();
+
+if size(args) < 2
+  disp("Numero de argumentos inválido!");
+  exit()
+endif
+filename = args{1};
+option = args{2};
+image = imread(filename);
+if strcmp(option, "-constrast")
+  finalimage = contrast(image);
+elseif strcmp(option, "-blur")
+  finalimage = blur(image);
+elseif strcmp(option, "-sharpen")
+  finalimage = sharpen(image);
+else
+  disp("Opção não reconhecida!");
+  exit();
+endif
+dotpos = size(filename)(2) - 3;
+finalfilename = filename;
+finalfilename(dotpos:dotpos+9) = "-final.jpg"
+imwrite (finalimage, finalfilename);
