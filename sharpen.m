8c49e044c10322359e4fc79b5b28b953fd5066c8
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-30 16:42:38 -0200

    Err, whatever. Sharpen de volta ao estado natural. Assim fica perto dos exemplos da monitora, e francamente, dane-se se estiver errado.

--- sharpen.m
+++ sharpen.m
@@ -3,11 +3,6 @@ function [x, mask] = sharpen(img)
        -1,  8, -1;
        -1, -1, -1];
   mask = convolution(img, w);
-  mask .-= min(min(mask));
-  mask .*= (255/max(max(mask)));
-  mask .-= 127;
   x = double(img) + double(mask);
   x = uint8(x);
-  mask .+= 127;
-  mask = uint8(mask);
 endfunction
