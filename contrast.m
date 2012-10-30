180cfca6fa5226208897154589760292e28da899
Renan T. Carneiro <renanteruoc@gmail.com>
2012-10-30 16:45:33 -0200

    Contrast agora devolve os histogramas

--- contrast.m
+++ contrast.m
@@ -1,13 +1,13 @@
-function x = contrast(img)
-  intensityProb(256) = 0;
+function [x, intensityCount, finalIntensityCount] = contrast(img)
+  intensityCount(256) = 0;
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
-      ++intensityProb(img(i, j)+1);
+      ++intensityCount(img(i, j)+1);
     endfor
   endfor
-    fda(1) = intensityProb(1);
+    fda(1) = intensityCount(1);
   for i = 2:256
-    fda(i) = fda(i-1) + intensityProb(i);
+    fda(i) = fda(i-1) + intensityCount(i);
   endfor
   fdamin = min(fda);
   for i = 1:256
@@ -17,6 +17,12 @@ function x = contrast(img)
   for i = 1:size(img)(1)
     for j = 1:size(img)(2)
       x(i, j) = h (x (i,j) + 1);
+    endfor
+  endfor
+  finalIntensityCount(256) = 0;
+  for i = 1:size(x)(1)
+    for j = 1:size(x)(2)
+      ++intensityCount(x(i, j)+1);
     endfor
   endfor
 endfunction
